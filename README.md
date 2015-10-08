41201f5ecc75962b48d528337757db9c8e557dbd
Anders Bergh <anders1@gmail.com>
2015-10-08 15:49:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# TelegramiTunes
+
+This brings the `%_itunes` feature from Adium to Telegram.
+`%_itunes` is replaced with the current artist and track name.
---
+++ TelegramiTunes.m
@@ -0,0 +1,73 @@
+// cc -o TelegramiTunes.dylib -dynamiclib TelegramiTunes.m  -framework Cocoa
+// env DYLD_INSERT_LIBRARIES=/Users/anders/TelegramiTunes.dylib \
+//   /Applications/Telegram.app/Contents/MacOS/Telegram
+
+#import <Cocoa/Cocoa.h>
+#import <objc/runtime.h>
+
+static NSString *lastKnownTrack = nil;
+static void (*originalIMP)(id, SEL, NSString *, id, dispatch_block_t) = NULL;
+
+static void override_sendMessage(id self, SEL _cmd, NSString *message,
+                                 id conversation, dispatch_block_t callback) {
+  if ([message rangeOfString:@"%_itunes" options:NSCaseInsensitiveSearch]
+          .location != NSNotFound) {
+    if (lastKnownTrack) {
+      message = [message
+          stringByReplacingOccurrencesOfString:@"%_itunes"
+                                    withString:lastKnownTrack
+                                       options:NSCaseInsensitiveSearch
+                                         range:NSMakeRange(0, [message length])];
+    }
+  }
+
+  originalIMP(self, _cmd, message, conversation, callback);
+}
+
+@interface TelegramiTunes : NSObject
+- (void)updateTrackInfo:(NSNotification *)notification;
+@end
+
+@implementation TelegramiTunes
+
+- (void)updateTrackInfo:(NSNotification *)notification {
+  NSDictionary *dict = [notification userInfo];
+
+  NSString *artist = dict[@"Artist"];
+  NSString *name = dict[@"Name"];
+  if (!artist || !name) {
+    return;
+  }
+
+  if (lastKnownTrack) {
+    [lastKnownTrack release];
+    lastKnownTrack = nil;
+  }
+
+  lastKnownTrack = [NSString stringWithFormat:@"%@ - %@", artist, name];
+  [lastKnownTrack retain];
+}
+
++ (void)load {
+  Class targetClass = NSClassFromString(@"MessagesViewController");
+  Method targetMethod = class_getInstanceMethod(
+      targetClass, @selector(sendMessage:forConversation:callback:));
+  originalIMP = (void *)method_getImplementation(targetMethod);
+
+  if (!class_addMethod(
+          targetClass, @selector(sendMessage:forConversation:callback:),
+          (IMP)override_sendMessage, method_getTypeEncoding(targetMethod))) {
+    method_setImplementation(targetMethod, (IMP)override_sendMessage);
+  }
+
+  TelegramiTunes *tgi = [[TelegramiTunes alloc] init];
+
+  NSDistributedNotificationCenter *dnc =
+      [NSDistributedNotificationCenter defaultCenter];
+  [dnc addObserver:tgi
+          selector:@selector(updateTrackInfo:)
+              name:@"com.apple.iTunes.playerInfo"
+            object:nil];
+}
+
+@end
